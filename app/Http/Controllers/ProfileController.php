2a5f932bdf1f4d5f2193cf6ac99a03b5c8cfd340
ARAhmedHridoy <arahmed.hridoy@gmail.com>
2024-06-22 19:48:20 +0600

    laravel project added

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProfileController extends Controller
+{
+    public function index($id){
+
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        $response = new Response(json_encode($data));
+        $response->header('Content-Type', 'application/json');
+        $response->withCookie(cookie($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly));
+
+        return $response;
+    }
+    
+}
